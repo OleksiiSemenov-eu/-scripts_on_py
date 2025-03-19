740da4d8cd41bb3eecca1b6a937476bd3f0b4a42
OleksiiSemenov-eu <oleksiisemenov.eu@gmail.com>
2025-03-19 16:54:49 +0100

    add comments

--- email and password.py
+++ email and password.py
@@ -9,7 +9,8 @@ def generate_random_email():
 def generate_random_password(length=10):
     return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
 
-count = 5                     
+count = 5
+# You can change this number to generate a different number of objects in the array
 
 data = []
 for _ in range(count):
@@ -19,7 +20,11 @@ for _ in range(count):
     }
     data.append(entry)
 
-filename = "email_and_password.json" 
+filename = "email_and_password.json"
+# Specify the path before 'email_and_password.json' to save the JSON file.
+# If you are using Windows, use double backslashes (\\) in the path.
+# If you are using Linux, use a forward slash (/).
+
 with open(filename, "w") as f:
     json.dump(data, f, indent=1)
 
